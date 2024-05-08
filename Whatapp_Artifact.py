4e36345d9423ce58d7f983a7c77f98c0a6de02d7
SamruddhiR21 <112927787+SamruddhiR21@users.noreply.github.com>
2024-05-08 11:04:49 +0530

    Update Whatapp_Artifact.py

--- Whatapp_Artifact.py
+++ Whatapp_Artifact.py
@@ -1,11 +1,11 @@
 import re
 import pandas as pd
 
-# Define the regex pattern to extract timestamp, sender, and message
+#Regex patterns to extract timestamp, sender, and message
 #pattern = r'(\d{2}/\d{2}/\d{4}, \d{2}:\d{2}) - (\w+): (.*)'
 pattern = r'^(\d{2}/\d{2}/\d{4}, \d{2}:\d{2}) - ([^:]+): (.*)$'
 
-# Read the text file and extract data using the regex pattern
+#Reading chat file
 data = []
 with open("C:\\Users\\S.DESKTOP-J9AB18Q\\Downloads\\WhatsApp Chat with Papa\\WhatsApp Chat with Papa.txt", 'r', encoding='utf-8') as file:
     for line in file:
@@ -14,8 +14,8 @@ with open("C:\\Users\\S.DESKTOP-J9AB18Q\\Downloads\\WhatsApp Chat with Papa\\Wha
             timestamp, sender, message = match.groups()
             data.append({'Timestamp': timestamp, 'Sender': sender, 'Message': message})
 
-# Create a DataFrame from the extracted data
+#Creating DataFrame
 df = pd.DataFrame(data)
 
-# Write the DataFrame to an Excel file
+#Output to Excel file
 df.to_excel("output_excel_file.xlsx", index=False)
